3b7d237ca978a0351b8a3aff965fa18d6ddce5f6
Travis Prol <travisjohnprol@gmail.com>
2020-05-06 20:56:43 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   counter = 0
   new_array = []
   while array.length <= counter
-    new_array.pusharray[counter] ** 2
+    new_array.push(array[counter] ** 2)
     counter += 1
   end
 end
